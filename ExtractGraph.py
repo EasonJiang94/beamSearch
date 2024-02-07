a69b902bf01cb7709a457f16ad0e299ec26f91a2
江宜欣 <eason@Eason.local>
2024-02-07 10:42:39 -0500

    init

---
+++ ExtractGraph.py
@@ -0,0 +1,11 @@
+class ExtractGraph:
+
+    # Please add comments along with your code.
+    # key is head word; value stores next word and corresponding probability.
+    graph = {}
+
+    sentences_add = "data//assign1_sentences.txt"
+
+    def __init__(self):
+        # Extract the directed weighted graph, and save to {head_word, {tail_word, probability}}
+        return
